0bba9c74e3fa75306bf0d7483541c90ec448e5e3
sadia <sadia98.seg@gmail.com>
2024-10-17 19:09:53 +0300

    Initial commit

---
+++ src/GuessingGame.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class GuessingGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // The correct name to guess
+        String correctName = "Emma";
+        String guess = "";
+        int attempts = 0;
+
+        // Keep asking until the correct guess or the user types "stop"
+        while (!guess.equalsIgnoreCase(correctName) && !guess.equalsIgnoreCase("stop")) {
+            System.out.println("Guess my name (type stop to exit)");
+            guess = scanner.nextLine();
+            attempts++;  // Increment the attempts each time
+
+            // Check if the guess is correct
+            if (guess.equalsIgnoreCase(correctName)) {
+                System.out.println("Congratulations!");
+            }
+        }
+
+        // Print how many times the user guessed
+        System.out.println("You guessed " + attempts + " times.");
+    }
+}
